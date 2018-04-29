e1f8474499f2c37a2fbbb3af8e58ba95c6a80778
Valentin Janvier <alencruserxx@hotmail.fr>
2018-04-29 23:01:33 +0200

    Changement d'emission de fonction par une pession sur touche entree plutot que bouton, inspiration sur un modele similaire au code de base

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 
 function submit () {
-	var input=document.getElementById('tache');
-	var checkbox=document.createElement('checkbox');
+	var input=document.getElementById('input').value;
+	var task=document.getElementById('tasks');
+	var li=document.createElement('li');
+
+	li.appendChild(document.createTextNode("" + input));
+	task.appendChild(li);
+	document.getElementById('input').value="";
+	li.onclick=removeItem;
+}
+	var entree = document.body;
+	entree.onkeyup = function (e){
+		if (e.keycode == 13){
+			newItem();
+		}
+	}
+/*var checkbox=document.createElement('checkbox');
 	checkbox.type='checkbox';
 	var li=document.createElement('li');
 	texte=document.createElement('label');
 
 	li.appendChild(checkbox);
-	li.appendChild(input);
-
-}
+	li.appendChild(input); */
